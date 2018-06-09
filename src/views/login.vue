388994d8d44a38ecfd8449ed88b7e98b5bbe206b
Kaysem <929200707@qq.com>
2018-06-09 20:13:58 +0800

    login页面

---
+++ src/views/login.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="login-wrap">
+  <el-form ref="form"
+  :model="form"
+  class="login-from"
+  label-width="80px"
+  label-position="top">
+    <h2>用户登录</h2>
+    <el-form-item label="用户名">
+      <el-input v-model="form.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="form.password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" class="login-btn">登 录</el-button>
+    </el-form-item>
+  </el-form>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-wrap .login-from {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+.login-wrap .login-from .login-btn {
+  width: 100%;
+}
+</style>
